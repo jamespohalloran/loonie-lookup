424b706199bffe498aff5da5d9d05b1271809952
James O'Halloran <james.p.ohalloran@gmail.com>
2025-02-01 10:26:08 -0400

    tiny layout tweak

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
     <footer className="bg-white">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
         <div className="flex justify-center gap-x-10"></div>
-        <div className="mt-10 text-sm/6 text-gray-600">
-          <div>
+        <div className="space-between mt-10 flex justify-center text-sm/6 text-gray-600">
+          <div className="m-auto">
             Made with ❤️ by <a href="http://johalloran.dev">James OHalloran.</a>
           </div>
           <div className="mt-2">
