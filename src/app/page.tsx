3af63fe4180eea7358b09d9c62ca717a5a6fcc48
James O'Halloran <james.p.ohalloran@gmail.com>
2025-02-01 10:08:36 -0400

    serverside search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,76 @@
 import Hero from '@/components/hero'
-import SearchResults from '@/components/search-results'
-import { getRecentOrders } from '@/data'
+import { SearchResults } from '@/components/search-results'
 
-export default async function Home() {
-  let orders = await getRecentOrders()
+export default async function Home(props: {
+  searchParams?: Promise<{
+    query?: string
+    page?: string
+  }>
+}) {
+  const searchParams = await props.searchParams
+  const query = searchParams?.query || ''
+
+  const filteredProducts = products.filter((product) => product.name.toLowerCase().includes(query.toLowerCase()))
 
   return (
     <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
       <Hero />
-      <div className="mt-8 flex items-end justify-between">
-        <div>
-          <h3 className="text-base font-semibold text-gray-900">🇨🇦</h3>
-
-          <SearchResults />
-        </div>
-        <div>
-          <h3 className="text-base font-semibold text-gray-900">👎</h3>
-
-          <SearchResults />
-        </div>
-      </div>
+      <SearchResults products={filteredProducts} />
     </div>
   )
 }
+
+const products = [
+  {
+    name: 'Heinz Ketchup',
+    email: 'leslie.alexander@example.com',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Michael Foster',
+    email: 'michael.foster@example.com',
+    role: 'Co-Founder / CTO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Dries Vincent',
+    email: 'dries.vincent@example.com',
+    role: 'Business Relations',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+  {
+    name: 'Lindsay Walton',
+    email: 'lindsay.walton@example.com',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Courtney Henry',
+    email: 'courtney.henry@example.com',
+    role: 'Designer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Tom Cook',
+    email: 'tom.cook@example.com',
+    role: 'Director of Product',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+]
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,4 +1,22 @@
+'use client'
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
 export default function Search() {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams)
+    if (term) {
+      params.set('query', term)
+    } else {
+      params.delete('query')
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }
+
   return (
     <div>
       <div className="mt-2">
@@ -8,6 +26,10 @@ export default function Search() {
           type="search"
           placeholder="Product name, brand, or category"
           className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+          onChange={(e) => {
+            handleSearch(e.target.value)
+          }}
+          defaultValue={searchParams.get('query')?.toString()}
         />
       </div>
     </div>
