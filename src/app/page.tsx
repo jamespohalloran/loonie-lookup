ccc68f09e3e75379c03fe789f3fbe92f0bf3ab53
James O'Halloran <james.p.ohalloran@gmail.com>
2025-02-01 09:50:41 -0400

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/hero'
+import SearchResults from '@/components/search-results'
 import { getRecentOrders } from '@/data'
 
 export default async function Home() {
@@ -7,7 +8,18 @@ export default async function Home() {
   return (
     <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
       <Hero />
-      <div className="mt-8 flex items-end justify-between"></div>
+      <div className="mt-8 flex items-end justify-between">
+        <div>
+          <h3 className="text-base font-semibold text-gray-900">🇨🇦</h3>
+
+          <SearchResults />
+        </div>
+        <div>
+          <h3 className="text-base font-semibold text-gray-900">👎</h3>
+
+          <SearchResults />
+        </div>
+      </div>
     </div>
   )
 }
